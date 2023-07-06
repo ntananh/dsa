577acc4883003e58d675ee413958db3f2568d2b4
Nguyen Tan Anh <tananh691@gmail.com>
2023-07-07 01:29:59 +0700

    feat(sort): add bubble sort implementation

---
+++ unhomie/src/main/java/algo/sort/ipml/BubbleSort.java
@@ -0,0 +1,29 @@
+package algo.sort.ipml;
+
+import algo.sort.Sort;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BubbleSort implements Sort<Integer> {
+    @Override
+    public List<Integer> sort(List<Integer> source) {
+        List<Integer> sorted = new ArrayList<>(source);
+
+        for (int i = 0; i < source.size(); i++) {
+            for (int j = i + 1; j < source.size(); j++) {
+                if (sorted.get(i) > sorted.get(j)) {
+                    swap(sorted, i, j);
+                }
+            }
+        }
+        return sorted;
+    }
+
+    public void swap(List<Integer> source, int i, int j) {
+        int valueAtI  = source.get(i);
+        int valueAtJ  = source.get(j);
+        source.set(i, valueAtJ);
+        source.set(j, valueAtI);
+    }
+}
