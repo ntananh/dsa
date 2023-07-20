5bb69d6cd2518c4f99a6b3def6990f5e0f090e64
Nguyen Tan Anh <anh.tan.nguyen@qa-systems.com>
2023-07-20 17:00:52 +0700

    chore(sort): tidy up bubble sort

--- unhomie/src/main/java/algo/sort/ipml/BubbleSort.java
+++ unhomie/src/main/java/algo/sort/ipml/BubbleSort.java
@@ -2,8 +2,6 @@ package algo.sort.ipml;
 
 import algo.sort.Sort;
 
-import java.util.Arrays;
-
 /**
  * Idea: elements with smaller values will float to the front of the array
  */
@@ -20,16 +18,8 @@ public class BubbleSort implements Sort {
     }
 
     public void swap(int[] source, int i, int j) {
-        int tmp  = source[i];
+        int tmp = source[i];
         source[i] = source[j];
         source[j] = tmp;
     }
-
-    public static void main(String[] args) {
-        int[] source = {5, 2, 1, 6, 7,0, 3, 4};
-        System.out.println("Before sort : " + Arrays.toString(source));
-        new BubbleSort().sort(source);
-        System.out.println("Sorted      : " + Arrays.toString(source));
-    }
-
 }
