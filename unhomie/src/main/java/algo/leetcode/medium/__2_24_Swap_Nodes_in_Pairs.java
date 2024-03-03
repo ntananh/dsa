0d1042a622b4f57138eb61d7c8d009acff9d72b1
Nguyen Tan Anh <tananh691@gmail.com>
2024-03-04 00:32:14 +0700

    chore(lc): refactoring

--- unhomie/src/main/java/algo/leetcode/medium/__2_24_Swap_Nodes_in_Pairs.java
+++ unhomie/src/main/java/algo/leetcode/medium/__2_24_Swap_Nodes_in_Pairs.java
@@ -1,74 +1,43 @@
 package algo.leetcode.medium;
 
 
+import algo.utils.Utils;
+import data.dto.ListNode;
+
 public class __2_24_Swap_Nodes_in_Pairs {
 
-    // * Definition for singly-linked list.
-    static class ListNode {
-        int val;
-        ListNode next;
+	public ListNode swapPairs(ListNode head) {
+		if (head == null) return null;
+		if (head.next == null) return head;
 
-        ListNode() {
-        }
+		// THTQ
+		ListNode nextNode = head.next;
 
-        ListNode(int val) {
-            this.val = val;
-        }
+		// Swap
+		head.next = nextNode.next;
+		nextNode.next = head;
 
-        ListNode(int val, ListNode next) {
-            this.val = val;
-            this.next = next;
-        }
+		head.next = swapPairs(head.next);
 
+		return nextNode;
 
-    }
+	}
 
-    static class Solution {
-        public ListNode swapPairs(ListNode head) {
-            if (head == null) return null;
-            if (head.next == null) return head;
+	public static void main(String[] args) {
+		__2_24_Swap_Nodes_in_Pairs s = new __2_24_Swap_Nodes_in_Pairs();
 
-            // THTQ
-            ListNode nextNode = head.next;
+		ListNode n1 = new ListNode(1);
+		ListNode n2 = new ListNode(2);
+		ListNode n3 = new ListNode(3);
+		ListNode n4 = new ListNode(4);
+		n1.next = n2;
+		n2.next = n3;
+		n3.next = n4;
 
-            // Swap
-            head.next = nextNode.next;
-            nextNode.next = head;
-
-            ListNode newNode = swapPairs(head.next);
-
-            head.next = newNode;
-
-            return nextNode;
-
-        }
-
-        public void printList(ListNode head) {
-            while (head != null) {
-                System.out.print(head.val + " ");
-                head = head.next;
-            }
-            System.out.println();
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution s = new Solution();
-
-        ListNode n1 = new ListNode(1);
-        ListNode n2 = new ListNode(2);
-        ListNode n3 = new ListNode(3);
-        ListNode n4 = new ListNode(4);
-        n1.next = n2;
-        n2.next = n3;
-        n3.next = n4;
-
-        s.printList(n1);
 
         ListNode newList = s.swapPairs(n1);
 
-        s.printList(newList);
-
-    }
+		Utils.printList(newList);
+	}
 
 }
