b275aa7bcf4dd6e91ff952f53258841dc2e8e4a4
Nguyen Tan Anh <tananh691@gmail.com>
2024-03-09 23:59:33 +0700

    feat(lc): add solution for 2540 minimum common value

---
+++ unhomie/src/main/java/algo/leetcode/easy/__14_2540_Minimum_Common_Value.java
@@ -0,0 +1,19 @@
+package algo.leetcode.easy;
+
+public class __14_2540_Minimum_Common_Value {
+	public int getCommon(int[] nums1, int[] nums2) {
+
+		int i = 0, j = 0;
+		while (i < nums1.length && j < nums2.length) {
+			if (nums1[i] == nums2[j]) {
+				return nums1[i];
+			} else if (nums1[i] < nums2[j]) {
+				i++;
+			} else {
+				j++;
+			}
+		}
+
+		return -1;
+	}
+}
