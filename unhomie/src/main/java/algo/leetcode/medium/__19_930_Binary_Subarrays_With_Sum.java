935fd5cbdf58f47c212e10c437b36e9372a2a519
Nguyen Tan Anh <tananh691@gmail.com>
2024-03-15 00:07:00 +0700

    fix(lc): rename function

--- unhomie/src/main/java/algo/leetcode/medium/__19_930_Binary_Subarrays_With_Sum.java
+++ unhomie/src/main/java/algo/leetcode/medium/__19_930_Binary_Subarrays_With_Sum.java
@@ -2,10 +2,10 @@ package algo.leetcode.medium;
 
 public class __19_930_Binary_Subarrays_With_Sum {
 	public int numSubarraysWithSum(int[] nums, int goal) {
-		return slidingwindow(nums, goal) - slidingwindow(nums, goal - 1);
+		return slidingWindow(nums, goal) - slidingWindow(nums, goal - 1);
 	}
 
-	private int slidingwindow(int[] nums, int goal) {
+	private int slidingWindow(int[] nums, int goal) {
 		int n = nums.length;
 		int left = 0, res = 0, sum = 0;
 		for (int right = 0; right < n; right++) {
