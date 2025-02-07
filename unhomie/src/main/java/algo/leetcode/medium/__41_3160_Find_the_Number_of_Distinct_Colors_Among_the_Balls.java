c57f5d759647249da69e1e7ed085b8f87923cae3
Nguyen Tan Anh <tananh691@gmail.com>
2025-02-07 19:17:00 +0900

    feat(lc): add solution for 3160 find the number of distinct colors among the balls

---
+++ unhomie/src/main/java/algo/leetcode/medium/__41_3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls.java
@@ -0,0 +1,37 @@
+package algo.leetcode.medium;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class __41_3160_Find_the_Number_of_Distinct_Colors_Among_the_Balls {
+
+    public int[] queryResults(int limit, int[][] queries) {
+        final int n = queries.length;
+        final int[] res = new int[n];
+        final Map<Integer, Integer> ballMap = new HashMap<>();
+        final Map<Integer, Integer> colorMap = new HashMap<>();
+
+        for (int[] query : queries) {
+            final int ball = query[0];
+            final int color = query[1];
+
+            if (ballMap.containsKey(ball)) {
+                final int prevColor = ballMap.get(ball);
+                colorMap.put(prevColor, colorMap.get(prevColor) - 1);
+
+                if (colorMap.get(prevColor) == 0) {
+                    colorMap.remove(prevColor);
+                }
+            }
+
+            ballMap.put(ball, color);
+            colorMap.put(color, colorMap.getOrDefault(color, 0) + 1);
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
