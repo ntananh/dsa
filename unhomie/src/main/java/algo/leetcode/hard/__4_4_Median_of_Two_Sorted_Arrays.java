bfe39118574e75330e9fbb23d70472830821aa29
Nguyen Tan Anh <tananh691@gmail.com>
2025-03-04 23:07:46 +0200

    wip(lc): find median sorted arrays

---
+++ unhomie/src/main/java/algo/leetcode/hard/__4_4_Median_of_Two_Sorted_Arrays.java
@@ -0,0 +1,39 @@
+package algo.leetcode.hard;
+
+public class __4_4_Median_of_Two_Sorted_Arrays {
+
+	/**
+	 * <p>
+	 * Given <b>two sorted arrays</b> {@code nums1 } and {@code nums2 }.</br>
+	 * And {@code nums1.length = m} and {@code nums2.length = n}
+	 * </p>
+	 *
+	 * <p> Return <b>the median</b> of the two sorted arrays. </p>
+	 *
+	 * <p><b>Note:</b> The overall run time complexity should be {@code O(log (m+n)) }. </p>
+	 *
+	 * <p><b>Constraints:</b></p>
+	 * <ul>
+	 *      <li>{@code nums1.length == m }</li>
+	 *      <li>{@code nums2.length == n }</li>
+	 *      <li>{@code 0 <= m <= 1000 }</li>
+	 *      <li>{@code 0 <= n <= 1000 }</li>
+	 *      <li>{@code 1 <= m + n <= 2000 }</li>
+	 *      <li>{@code -106 <= nums1[i], nums2[i] <= 106 }</li>
+	 * </ul>
+	 *
+	 * @param nums1 first array
+	 * @param nums2 second array
+	 * @return the median of 2 arrays
+	 */
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int[] medians = new int[2];
+
+
+		return -1;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
